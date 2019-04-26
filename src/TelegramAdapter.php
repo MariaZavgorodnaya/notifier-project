4f0d85fc2eed602fe9a7188334296b71cc48134b
MashaZ <oliverum-oliverum@yandex.ru>
2019-04-26 18:31:33 +0300

    Bug in Telegram Adapter

--- src/TelegramAdapter.php
+++ src/TelegramAdapter.php
@@ -11,53 +11,66 @@ namespace loggers;
 
 class TelegramAdapter implements AdapterInterface
 {
+    protected $chat_id;
+    protected $token;
 
-
+public function __construct($chat_id, $token){
+    $this->chat_id = $chat_id;
+    $this->token = $token;
+}
     public function debug($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function info($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function notice($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function warning($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function error($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function critical($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function alert($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
     public function emergency($message)
     {
-        $url = "https://api.telegram.org/bot852833193:AAEPCTmREUAffAdm-0_ePJ_bLgHSHcuBwA4/getMe";
+        $url = "https://api.telegram.org/bot$this->token/sendMessage?
+        chat_id=$this->chat_id&text=$message";
         file_get_contents($url . $message);
     }
 
